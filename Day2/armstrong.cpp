185139bc111fd53c346c5e9b08bdf2312e5f6185
GautamiSanap21 <sanapgautami2107@gmail.com>
2025-10-20 09:53:43 +0530

    Question4

---
+++ Day2/armstrong.cpp
@@ -0,0 +1,31 @@
+//Question 4
+//Check if number is Armstrong or not
+
+#include<iostream>
+using namespace std;
+
+// Function to check if a number is an Armstrong number
+bool isArmstrong(int num) {
+    int num1 = num;
+    int sum = 0;
+
+    while (num != 0) {
+        int rem = num % 10;
+        sum += rem * rem * rem;
+        num /= 10;
+    }
+
+    return sum == num1;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isArmstrong(num)) {
+        cout << "The number is an Armstrong number" << endl;
+    } else {
+        cout << "The number is not an Armstrong number" << endl;
+    }
+}
